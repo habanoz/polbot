d2bea09734ad1e622bb7c0fa237778451919e10f
EMIN YUCE <eminyuce@gmail.com>
2017-04-11 17:11:09 -0400

    Ajax Method requesting

--- polbot-core/src/main/java/com/habanoz/polbot/core/web/controller/AjaxController.java
+++ polbot-core/src/main/java/com/habanoz/polbot/core/web/controller/AjaxController.java
@@ -4,11 +4,10 @@ import com.habanoz.polbot.core.entity.CurrencyConfig;
 import com.habanoz.polbot.core.repository.BotUserRepository;
 import com.habanoz.polbot.core.repository.CurrencyConfigRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -16,6 +15,7 @@ import java.util.Map;
  * Created by Yuce on 4/11/2017.
  */
 @Controller
+
 public class AjaxController {
 
     @Autowired
@@ -24,12 +24,24 @@ public class AjaxController {
     @Autowired
     private CurrencyConfigRepository currencyConfigRepository;
 
-    @RequestMapping(value = "/ajax/GetUserCurrencies",method = RequestMethod.POST)
-    public @ResponseBody  List<CurrencyConfig> GetUserCurrencies(@RequestParam("userId") int userId) {
+    @RequestMapping(value = "/GetAjaxUsers", method = RequestMethod.POST)
+    @ResponseBody
+    public String GetUserCurrencies(@RequestParam("userId") int userId) {
 
        // int userId=1;
         List<CurrencyConfig> userCurrencies = currencyConfigRepository.findByUserId(userId);
        // Returned the render html page from over here.
-        return userCurrencies;
+        return "test";
     }
+
+
+//    @RequestMapping(value = "/GetAjaxUsers", method = RequestMethod.GET)
+//    @ResponseBody
+//    public List<CurrencyConfig> GetUserCurrencies(@RequestParam("userId") int userId) {
+//
+//        // int userId=1;
+//        List<CurrencyConfig> userCurrencies = currencyConfigRepository.findByUserId(userId);
+//        // Returned the render html page from over here.
+//        return userCurrencies;
+//    }
 }
